c48e26441462a1568cdf76ed01d4df7570cdb915
Mahsa Pesaran <mahsa.pesaran@gmail.com>
2023-10-05 11:59:33 +1100

    add comments to clarify each part of the code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Task from './components/Task';
+import Task from './components/Task'; //Import Task component
 import {
   KeyboardAvoidingView,
   StyleSheet,
@@ -13,10 +13,16 @@ import {
 //The AsyncStorage is the package we use for storing and retrieving data persistently.
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
 
+// Define the main component
 export default function App() {
+  // Define states to manage tasks and the input field
   const [task, setTask] = useState('');
   const [taskItems, setTaskItems] = useState([]);
 
+  /* I used a useEffect hook to load tasks from AsyncStorage when the component mounts. 
+  Inside the loadTasks function, I used AsyncStorage.getItem('tasks') to retrieve the saved tasks as a string.
+  then checked if there are saved tasks, parse them into an array with JSON.parse, and set the taskItems state 
+  with the retrieved tasks.*/
   useEffect(() => {
     async function loadTasks() {
       try {
@@ -32,6 +38,9 @@ export default function App() {
     loadTasks();
   }, []);
 
+  /*I used another useEffect hook to save tasks to AsyncStorage whenever taskItems changes.
+  Inside the saveTasks function, I used AsyncStorage.setItem('tasks', JSON.stringify(taskItems)) to save the 
+  taskItems array as a JSON string in AsyncStorage. */
   useEffect(() => {
     async function saveTasks() {
       try {
@@ -44,6 +53,10 @@ export default function App() {
     saveTasks();
   }, [taskItems]);
 
+
+  /* The handleAddTask function is responsible for adding a new task.
+  It dismisses the keyboard using Keyboard.dismiss().
+  If the task input is not empty, it adds the task to the taskItems array and clears the input field.*/
   const handleAddTask = () => {
     Keyboard.dismiss();
     if (task) {
@@ -52,38 +65,53 @@ export default function App() {
     }
   };
 
+  /*The handleDeleteTask function is responsible for deleting a task.
+  It creates a new array newTaskItems by cloning taskItems.
+  It removes the task at the specified index using splice,
+  then sets taskItems to the new array, effectively removing the task. */
   const handleDeleteTask = (index) => {
     const newTaskItems = [...taskItems];
     newTaskItems.splice(index, 1);
     setTaskItems(newTaskItems);
   };
 
+  // Return the main app interface
   return (
     <View style={styles.container}>
+      {/* Task List Section */}
         <View style={styles.taskWrapper}>
           <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+          {/*Instead of mapping over taskItems, I used a FlatList to render the list of tasks.
+            data={taskItems} specifies the data source for the list.
+            keyExtractor defines a unique key for each item.
+            renderItem specifies how each item is rendered, including the Task component and the onPress event to delete a task. */}
           <FlatList
             style={styles.taskList} 
             data={taskItems}
             keyExtractor={(item, index) => index.toString()}
             renderItem={({ item, index }) => (
               <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+                {/*The text prop is passed to the Task component to display the task description. */}
                 <Task text={item} />
               </TouchableOpacity>
             )}
           />
         </View>
 
+      {/* Task Input Section */}     
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.writeTaskWrapper}
       >
+        {/* Input field for adding tasks */}
         <TextInput
           style={styles.input}
           placeholder={'Write a task'}
           value={task}
           onChangeText={(text) => setTask(text)}
         />
+        {/* Button to add a task */}
         <TouchableOpacity onPress={handleAddTask}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
@@ -94,6 +122,7 @@ export default function App() {
   );
 }
 
+// Define styles for the components
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- components/Task.js
+++ components/Task.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
 
+/*Task component is designed to represent a single task item with a left side 
+for task description and a right side for a completion indicator*/
 
 const Task = (props) => {
     return (
         <View style= {styles.item}>
+            {/*This is a nested <View> component that contains the left side of the task item.
+            Inside this view, there's a square (<View style={styles.square}></View>) that can be
+            used as a checkbox or an indicator.
+            Next to the square, there's a <Text> component displaying the task description ({props.text}). */}
             <View style={styles.itemLeft}>
                 <View style = {styles.square}></View>
                 <Text styles={styles.itemText}>{props.text}</Text>
             </View>
+            {/*This is a <View> component representing the right side of the task item.
+            Inside this view, there's a circular shape that can be used as a completion indicator or any other visual element. */}
             <View style={styles.circular}></View>
         </View>
     )
